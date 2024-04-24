0274441633a2e2468a140902a15b7ca35e973112
angela <oranger215@gmail.com>
2024-04-24 19:25:08 +0900

    'uwu'

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,16 @@
-const mysql = require('mysql2')
-require('dotenv').config()
+const mysql = require('mysql2');
+require('dotenv').config();
 
-const pool = mysql.createPool(process.env.JAWSDB_URL)
-module.exports = pool.promise()
+console.log("JAWSDB_URL:", process.env.JAWSDB_URL); // Логируем строку подключения
+
+// Проверка на наличие строки подключения
+if (!process.env.JAWSDB_URL) {
+    console.error("Не определена переменная окружения JAWSDB_URL");
+    process.exit(1); // Завершение процесса с ошибкой
+}
+
+const pool = mysql.createPool({
+    uri: process.env.JAWSDB_URL
+});
+
+module.exports = pool.promise();
