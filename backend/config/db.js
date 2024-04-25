557c538cb7d0c36ec1d4b93700cc057415924b0d
angela <oranger215@gmail.com>
2024-04-25 17:10:38 +0900

    'o-o'

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+/* const mysql = require('mysql2');
 require('dotenv').config();
 
 console.log("JAWSDB_URL:", process.env.JAWSDB_URL); // Логируем строку подключения
@@ -20,3 +20,47 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
+ */
+ 
+ 
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://dmitrieva:qwer1234@185.250.46.244:27017/db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('Connection error', err);
+});
+
+const app = express();
+
+// Middleware для разбора JSON тел запросов
+app.use(express.json());
+
+// Определение схемы и модели пользователя
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  // Другие поля...
+});
+const User = mongoose.model('User', userSchema);
+
+// Маршруты
+app.post('/users', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Ошибка при создании пользователя', error });
+  }
+});
+
+// Слушать на порту
+const PORT = process.env.PORT || 3265;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
