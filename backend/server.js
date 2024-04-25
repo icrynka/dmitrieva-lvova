8ba572dd143b791db5e84662112adb9eb5b1dda5
icrynka <icrynka@gmail.com>
2024-04-25 16:00:02 +0900

    port2

--- backend/server.js
+++ backend/server.js
@@ -83,7 +83,7 @@ io.on('connection', (socket) => {
   })
 })
 
-const PORT = process.env.PORT || 3298
+const PORT = process.env.PORT || 3265
 
 server.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`)
